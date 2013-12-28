f5e0c8fa3edab84046b3e432701b68919b9b40be
Go Wind <govindappumenon@gmail.com>
2013-12-28 16:06:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nine61::Application.config.secret_key_base = '8f015f431572df44fbb86f49990c40b8bbeb32008aef0cd9c0a89c74c168dd73f4cc4b105021cbe48fb11939b71cd20758060a03f6faf32d0b0125f71624c33d'
